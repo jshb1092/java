739318b78a8b1ad8522f8457333917f7169702a2
jshb1092 <161662574+jshb1092@users.noreply.github.com>
2024-03-08 09:09:03 +0900

    Leeyt

--- studyJava/src/Leeyt16.java
+++ studyJava/src/Leeyt16.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
+
 
 public class Leeyt16 {
     public static void main(String[] args) {
@@ -9,16 +13,32 @@ public class Leeyt16 {
 
         //9.1~45까지의 랜덤한 정수를 length가 6인 배열에 넣으시오.
         //단,중복 숫자는 허용하지 않음!!
-        int[] numberlist1 ={1,2,3,4,5,6};
-        int[] numberlist2 ={0,1,2};
-        for (int i=0;i<numberlist1.length;i++){
-           numberlist1[i]=numberlist1[i]-1;
-           for(int j=0;j<numberlist2.length;j++){
-               numberlist2[j]=numberlist2[j]+1;
-           }
-        }
         Random rd = new Random();
-        int randNum = rd.nextInt(45)+1;
+        int randNum = rd.nextInt(10); // 0 부터 입력한 (정수값-1) 범위에서 랜덤
         System.out.println(randNum);
+        Random randClass = new Random();
+        int[] rottoList = new int[6];
+
+//        for (int i=0; i<rottoList.length; i++) {
+//            int temp = randClass.nextInt(45) + 1;
+//            for (int j=0; j<i; j++) {
+//                if (rottoList[j] == temp) {
+//                    temp = randClass.nextInt(45) + 1;
+//                    j = -1;
+//                }
+//            }
+//            rottoList[i] = temp;
+//        }
+//        System.out.println(Arrays.toString(rottoList));
+
+        for (int i=0; i<rottoList.length; i++) {
+            rottoList[i] = randClass.nextInt(45) + 1;
+            for (int j=0; j<i; j++) {
+                if (rottoList[j] == rottoList[i]) {
+                    i = i - 1;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(rottoList));
     }
 }
